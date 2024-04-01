3ef59323bcb8775862691a33a4fe54d0c34074ea
Luis Eduardo <luiseperezp.10@gmail.com>
2024-03-31 23:41:08 -0500

    feat(component): add footer component

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../../components/header/header.component";
+import { FooterComponent } from "../../components/footer/footer.component";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
-  imports: [HeaderComponent]
+  imports: [HeaderComponent, FooterComponent]
 })
 export class HomePageComponent {
 
