00bae2f1bdabb2a6121c127bd26dbd4d9670656c
Luis Eduardo <luiseperezp.10@gmail.com>
2024-04-04 20:40:02 -0500

    feat(component): add filter community by city

--- src/app/pages/home-page/home-page.component.ts
+++ src/app/pages/home-page/home-page.component.ts
@@ -5,13 +5,14 @@ import { CardCommunityDetailComponent } from "../../components/card-community-de
 import { dataCommunities } from '../../../../_data/communities';
 import { ICommunity } from '../../../../model/community.model';
 import { CardContributorsComponent } from "../../components/card-contributors/card-contributors.component";
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss',
-  imports: [HeaderComponent, FooterComponent, CardCommunityDetailComponent, CardContributorsComponent]
+  imports: [HeaderComponent, FooterComponent, CardCommunityDetailComponent, CardContributorsComponent, FormsModule]
 })
 export class HomePageComponent {
   communityList = dataCommunities;
@@ -27,5 +28,13 @@ export class HomePageComponent {
       cities.add(item.city);
     });
     return Array.from(cities);
+  }
+
+  filterByCity(event: any) {
+    if (event.target.value === 'all') {
+      this.communityList = dataCommunities;
+      return;
+    }
+    this.communityList = dataCommunities.filter((item: ICommunity) => item.city === event.target.value);
   }
 }
