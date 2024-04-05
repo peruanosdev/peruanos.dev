d84ea2ba3a48fc1053329a5491b2fcfb72e62ab0
Luis Eduardo <luiseperezp.10@gmail.com>
2024-04-04 23:40:44 -0500

    refactor(deploy): update routes and path

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,9 @@ import { HomePageComponent } from './pages/home-page/home-page.component';
 
 export const routes: Routes = [{
     path: '',
-    redirectTo: '/',
-    pathMatch: 'full',
-}, {
-    path: '/',
     component: HomePageComponent,
 }, {
     path: '**',
-    redirectTo: 'home',
+    redirectTo: '',
     pathMatch: 'full',
 }];
